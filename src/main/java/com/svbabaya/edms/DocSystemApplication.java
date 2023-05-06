012cb02214456de0df069489d704cd083953310e
svbabaya <babayan.serge@gmail.com>
2023-05-07 01:43:56 +0300

    First commit

---
+++ src/main/java/com/svbabaya/edms/DocSystemApplication.java
@@ -0,0 +1,14 @@
+package com.svbabaya.edms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DocSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svbabaya/edms/EdmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svbabaya.edms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
