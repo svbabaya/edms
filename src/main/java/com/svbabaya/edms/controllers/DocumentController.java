ee1ed3c499cacd245fbc27922d213724572554f7
svbabaya <babayan.serge@gmail.com>
2023-05-07 13:31:43 +0300

    Add DocumentController

---
+++ src/main/java/com/svbabaya/edms/controllers/DocumentController.java
@@ -0,0 +1,11 @@
+package com.svbabaya.edms.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class DocumentController {
+    @GetMapping
+    public String start() {
+        return "hi spring";
+    }
+}
